034d9154bbd97c74c42753646e147b4237027e9c
davide-piarulli <davide.piarulli.1@gmail.com>
2024-05-02 13:56:46 +0100

    [add] costruttore

--- Model/Movies.php
+++ Model/Movies.php
@@ -1,3 +1,17 @@
 <?php
 
-?>
+class Movie
+{
+  public $title;
+  public $plot;
+  public $genre;
+  public $rating;
+
+  public function __construct(string $_title, string $_plot, string $_genre, float $_rating)
+  {
+    $this->title = $_title;
+    $this->plot = $_plot;
+    $this->genre = $_genre;
+    $this->rating = $_rating;
+  }
+}
--- index.php
+++ index.php
@@ -1,11 +1,17 @@
+<?php
+require_once __DIR__ . '/Model/Movies.php'
+?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Document</title>
+  <title>PHP OOP</title>
 </head>
+
 <body>
-  
+
 </body>
+
 </html>
